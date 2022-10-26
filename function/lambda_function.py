ef19e348324d94c79e9e6580edd8e1276f5f5dc6
Victor Amadi <benson_amadi@yahoo.com>
2022-10-26 10:54:46 +0200

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -8,8 +8,9 @@ def lambda_handler(event, context):
     Returns:
         string: greeting response
     """
-    print('Starting functions\n---------------------------------------------'
+    print('Starting functions\n---------------------------------------------')
 
+    
     if event["input"] == "Hello":
 
         return "World"
